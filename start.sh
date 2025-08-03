374af2693c44fcadbfcd8110c057cd890e4f7f8a
descarado10 <mukimov0209@gmail.com>
2025-08-04 00:03:44 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Installing PyTorch manually..."
+pip install torch==1.13.1+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+
 echo "Starting bot..."
 python bot.py
+
